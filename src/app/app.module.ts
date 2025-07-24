eda9b04bc26a06e68d499f8da71dd3d033bbc526
Ferdous Adit <ferdous.adit@catalisgov.com>
2025-07-24 17:30:18 -0600

    working on the CRUD operations, currently need to finish the delete operation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductModule } from './product/product.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ProductModule } from './product/product.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ProductModule
+    ProductModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';    
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private productUrl = 'assets/projects.json';
+  private products: Product[] = []
+
+  constructor(private http: HttpClient) { }
+  
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.productUrl);
+  }
+
+  getProduct(id: number): Observable<Product> {
+    return this.http.get<Product>(`${this.productUrl}/${id}`);
+  }
+
+  createProduct(product: Product): Observable<Product> {
+    product.id = Date.now();
+    return this.http.post<Product>(this.productUrl, product);
+  }
+
+  updateProduct(product: Product): Observable<Product> {
+    const url = `${this.productUrl}/${product.id}`;
+    return this.http.put<Product>(url, product);
+  }
+
+  }
 }
