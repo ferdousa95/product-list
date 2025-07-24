610232662700673aa0492a5783b8ec10f5765e8b
Ferdous Adit <ferdous.adit@catalisgov.com>
2025-07-24 14:03:45 -0600

    router is setup, and then have to work on the product service and the product-list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: 'products', component: ProductListComponent },
+  { path: 'products/:id', component: ProductListComponent }, 
+  { path: '**', redirectTo: '/products' } // Wildcard route for a 404 page or redirect
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
